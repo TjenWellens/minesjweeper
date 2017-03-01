a2ef6ca721bc9d74aec2d53298537a71f875eaf8
Tjen Wellens <(none)>
2017-03-01 21:46:14 +0100

    1,1 = 0

--- test/test.server.js
+++ test/test.server.js
@@ -3,3 +3,15 @@ var expect = require('chai').expect;
 it('true should equal true', function () {
   expect(true).to.eql(true);
 });
+
+// bommen, cel 4-5 => # bommen in de buurt
+
+describe('empty game', function () {
+  it('(1,1) should return 0', function () {
+    expect(play(1, 1)).to.eql(0);
+  });
+});
+
+function play (x, y) {
+  return 0;
+}
