59e699673798395c427aec31e1c88b4b772e3c7e
Tjen Wellens <(none)>
2017-03-01 22:03:06 +0100

    refactor

--- test/test.server.js
+++ test/test.server.js
@@ -8,34 +8,34 @@ it('true should equal true', function () {
 describe('empty game', function () {
   it('(1,1) should return 0', function () {
     var bombs = [];
-    expect(play(1, 1, bombs)).to.eql(0);
+    expect(play({x: 1, y: 1}, bombs)).to.eql(0);
   });
 });
 
 describe('game with bomb on {1,1}', function () {
   var bombs = [{x: 1, y: 1}];
   it('(1,1) should return GAME OVER', function () {
-    expect(play(1, 1, bombs)).to.eql('GAME OVER');
+    expect(play({x: 1, y: 1}, bombs)).to.eql('GAME OVER');
   });
   it('(1,2) should return 1', function () {
-    expect(play(1, 2, bombs)).to.eql(1);
+    expect(play({x: 1, y: 2}, bombs)).to.eql(1);
   });
   it('(2,1) should return 1', function () {
-    expect(play(2, 1, bombs)).to.eql(1);
+    expect(play({x: 2, y: 1}, bombs)).to.eql(1);
   });
 });
 
 describe('game with bomb on {1,2}', function () {
   var bombs = [{x: 1, y: 2}];
   it('(1,2) should return GAME OVER', function () {
-    expect(play(1, 2, bombs)).to.eql('GAME OVER');
+    expect(play({x: 1, y: 2}, bombs)).to.eql('GAME OVER');
   });
 });
 
-function play (x, y, bombs) {
+function play (coord, bombs) {
   if (bombs.length === 0)
     return 0;
-  if (bombs[0].x === x && bombs[0].y === y)
+  if (bombs[0].x === coord.x && bombs[0].y === coord.y)
     return 'GAME OVER';
   return 1;
 }
